a2712ee1c75c2982ee3f5e58a34293f16664ab6b
Takashi Hirata <silverfilain@gmail.com>
2010-09-12 02:06:43 +0900

    Improve audiomuxer to be more user friendly.
    Check options and inputs more strictly and report if incompatible.

--- audiomuxer.c
+++ audiomuxer.c
@@ -45,15 +45,18 @@ static void cleanup_structs( structs_t* structs )
         mp4sys_importer_close( structs->importer );
 }
 
-static int m4amux_error( structs_t* structs, char* msg )
+static int audiomux_error( structs_t* structs, char* msg )
 {
     cleanup_structs( structs );
     eprintf( msg );
     return -1;
 }
 
-#define M4AMUX_ERR( msg ) m4amux_error( &structs, msg )
-#define M4AMUX_USAGE_ERR() M4AMUX_ERR( "Usage: m4amuxer [--sbr] [--3gp|--3g2] input output\n" )
+#define AUDIOMUX_ERR( msg ) audiomux_error( &structs, msg )
+#define AUDIOMUX_USAGE_ERR() AUDIOMUX_ERR( \
+    "Usage: audiomuxer [--sbr] [--3gp|--3g2] [--m4a] input output\n" \
+    "       If both --3gp/--3g2 and --m4a are used, latter one takes major brand.\n" \
+)
 
 int main( int argc, char* argv[] )
 {
@@ -61,15 +64,18 @@ int main( int argc, char* argv[] )
     structs_t structs = { 0 };
     int sbr = 0;
     int brand_3gx = 0;
+    int m4a = 0;
+    uint32_t brands[5] = { ISOM_BRAND_TYPE_ISOM, ISOM_BRAND_TYPE_MP42, 0, 0, 0 };
+    uint32_t major_brand = ISOM_BRAND_TYPE_MP42;
+    int num_of_brands = 2;
+    uint32_t minor_version = 1;
 
     if( argc < 3 )
-        return M4AMUX_USAGE_ERR();
+        return AUDIOMUX_USAGE_ERR();
 
     int i = 1;
-    while( *argv[i] == '-' )
+    while( argc > i && *argv[i] == '-' )
     {
-        if( argc <= i + 2 )
-            return M4AMUX_USAGE_ERR();
         if( !strcasecmp( argv[i], "--sbr" ) )
         {
             sbr = 1;
@@ -77,23 +83,46 @@ int main( int argc, char* argv[] )
         }
         else if( !strcasecmp( argv[i], "--3gp" ) )
         {
+            if( brand_3gx )
+                return AUDIOMUX_USAGE_ERR();
             brand_3gx = 1;
+            major_brand = ISOM_BRAND_TYPE_3GP6;
+            brands[num_of_brands++] = ISOM_BRAND_TYPE_3GP6;
+            minor_version = 0x00000000; /* means, 3gp(3gp6) 6.0.0 : "6" is not included in minor_version. */
             eprintf( "Using 3gp muxing mode.\n" );
         }
         else if( !strcasecmp( argv[i], "--3g2" ) )
         {
+            if( brand_3gx )
+                return AUDIOMUX_USAGE_ERR();
             brand_3gx = 2;
+            major_brand = ISOM_BRAND_TYPE_3G2A;
+            brands[num_of_brands++] = ISOM_BRAND_TYPE_3GP6;
+            brands[num_of_brands++] = ISOM_BRAND_TYPE_3G2A;
+            minor_version = 0x00010000; /* means, 3g2(3g2a) 1.0.0 : a == 1 */
             eprintf( "Using 3g2 muxing mode.\n" );
         }
+        else if( !strcasecmp( argv[i], "--m4a" ) )
+        {
+            if( m4a )
+                return AUDIOMUX_USAGE_ERR();
+            m4a = 1;
+            major_brand = ISOM_BRAND_TYPE_M4A;
+            brands[num_of_brands++] = ISOM_BRAND_TYPE_M4A;
+            minor_version = 1;
+            eprintf( "Using m4a muxing mode.\n" );
+        }
         else
-            return M4AMUX_USAGE_ERR();
+            return AUDIOMUX_USAGE_ERR();
         i++;
     }
+    if( argc != i + 2 )
+        return AUDIOMUX_USAGE_ERR();
 
     /* Initialize importer framework */
     structs.importer = mp4sys_importer_open( argv[i++], "auto" );
     if( !structs.importer || !(structs.summary = mp4sys_duplicate_audio_summary( structs.importer, 1 )) )
-        return M4AMUX_ERR( "Failed to open input file.\n" );
+        return AUDIOMUX_ERR( "Failed to open input file.\n" );
 
     /* check codec type. */
     enum isom_codec_code codec_code;
@@ -104,71 +133,67 @@ int main( int argc, char* argv[] )
     case MP4SYS_OBJECT_TYPE_Audio_ISO_11172_3: /* Legacy Interface */
         codec_code = ISOM_CODEC_TYPE_MP4A_AUDIO; break;
     case MP4SYS_OBJECT_TYPE_PRIV_SAMR_AUDIO:
+        if( m4a )
+            return AUDIOMUX_ERR( "The input seems AMR-NB but it's not compatible with --m4a.\n" );
         codec_code = ISOM_CODEC_TYPE_SAMR_AUDIO; break;
     case MP4SYS_OBJECT_TYPE_PRIV_SAWB_AUDIO:
+        if( m4a )
+            return AUDIOMUX_ERR( "The input seems AMR-WB but it's not compatible with --m4a.\n" );
         codec_code = ISOM_CODEC_TYPE_SAWB_AUDIO; break;
     default:
-        return M4AMUX_ERR( "Unknown object_type_indication.\n" );
+        return AUDIOMUX_ERR( "Unknown object_type_indication.\n" );
     }
     /* user defined sbr mode. */
     if( sbr )
     {
         if( structs.summary->object_type_indication != MP4SYS_OBJECT_TYPE_Audio_ISO_14496_3 )
-            return M4AMUX_ERR( "Input file is not MP4A.\n" );
+            return AUDIOMUX_ERR( "--sbr is only valid with MP4A.\n" );
         structs.summary->sbr_mode = MP4A_AAC_SBR_BACKWARD_COMPATIBLE;
         if( mp4sys_setup_AudioSpecificConfig( structs.summary ) )
-            return M4AMUX_ERR( "Failed to set SBR mode.\n" );
+            return AUDIOMUX_ERR( "Failed to set SBR mode.\n" );
     }
     /* user defined brand mode. */
     if( brand_3gx )
     {
         if( structs.summary->frequency > 48000
             || ( sbr && structs.summary->frequency > 24000 ) )
-            return M4AMUX_ERR( "3gp/3g2 does not allow frequency > 48000.\n" );
+            return AUDIOMUX_ERR( "3gp/3g2 does not allow frequency > 48000.\n" );
         if( structs.summary->channels > 2 )
-            return M4AMUX_ERR( "3gp/3g2 does not allow channels > 2.\n" );
+            return AUDIOMUX_ERR( "3gp/3g2 does not allow channels > 2.\n" );
     }
 
     /* Initialize L-SMASH muxer */
     structs.root = isom_create_movie( argv[i++] );
     if( !structs.root )
-        return M4AMUX_ERR( "Failed to create root.\n" );
+        return AUDIOMUX_ERR( "Failed to create root.\n" );
 
     uint32_t track = isom_create_track( structs.root, ISOM_HDLR_TYPE_AUDIO );
     if( !track )
-        return M4AMUX_ERR( "Failed to create a track.\n" );
+        return AUDIOMUX_ERR( "Failed to create a track.\n" );
 
     if( isom_set_max_chunk_duration( structs.root, 0.5 ) )
-        return M4AMUX_ERR( "Failed to set max duration per chunk.\n" );
+        return AUDIOMUX_ERR( "Failed to set max duration per chunk.\n" );
 
     if( isom_set_movie_timescale( structs.root, 600 ) )
-        return M4AMUX_ERR( "Failed to set movie timescale.\n" );
+        return AUDIOMUX_ERR( "Failed to set movie timescale.\n" );
 
     /* Initialize track */
     if( isom_set_media_timescale( structs.root, track, structs.summary->frequency ) )
-        return M4AMUX_ERR( "Failed to set media timescale.\n" );
+        return AUDIOMUX_ERR( "Failed to set media timescale.\n" );
 
     char handler_name[24] = "L-SMASH Audio Handler 1";
     if( isom_set_handler_name( structs.root, track, handler_name ) )
-        return M4AMUX_ERR( "Failed to set handler name.\n" );
+        return AUDIOMUX_ERR( "Failed to set handler name.\n" );
 
     uint32_t sample_entry = isom_add_sample_entry( structs.root, track, codec_code, structs.summary );
     if( !sample_entry )
-        return M4AMUX_ERR( "Failed to add sample_entry.\n" );
+        return AUDIOMUX_ERR( "Failed to add sample_entry.\n" );
 
     /* Preparation for writing */
-    uint32_t brands[5] = { ISOM_BRAND_TYPE_ISOM, ISOM_BRAND_TYPE_MP42, ISOM_BRAND_TYPE_M4A, ISOM_BRAND_TYPE_3GP6, ISOM_BRAND_TYPE_3G2A };
-    int major_index = 2 + brand_3gx;
-    int num_of_brands = 3 + brand_3gx;
-    uint32_t minor_version = 1;
-    if( brand_3gx == 1 )
-        minor_version = 0x00000000; /* means, 3gp(3gp6) 6.0.0 : "6" is not included in minor_version. */
-    else
-        minor_version = 0x00010000; /* means, 3g2(3g2a) 1.0.0 : a == 1 */
-    if( isom_set_brands( structs.root, brands[major_index], minor_version, brands, num_of_brands ) || isom_write_ftyp( structs.root ) )
-        return M4AMUX_ERR( "Failed to set brands.\n" );
+    if( isom_set_brands( structs.root, major_brand, minor_version, brands, num_of_brands ) || isom_write_ftyp( structs.root ) )
+        return AUDIOMUX_ERR( "Failed to set brands.\n" );
     if( isom_add_mdat( structs.root ) )
-        return M4AMUX_ERR( "Failed to write mdat.\n" );
+        return AUDIOMUX_ERR( "Failed to write mdat.\n" );
 
     /* transfer */
     uint32_t numframe = 0;
@@ -178,7 +203,7 @@ int main( int argc, char* argv[] )
         /* allocate sample buffer */
         isom_sample_t *sample = isom_create_sample( structs.summary->max_au_length );
         if( !sample )
-            return M4AMUX_ERR( "Failed to alloc memory for buffer.\n" );
+            return AUDIOMUX_ERR( "Failed to alloc memory for buffer.\n" );
         /* read a audio frame */
         /* FIXME: mp4sys_importer_get_access_unit() returns 1 if there're any changes in stream's properties.
            If you want to support them, you have to retrieve summary again, and make some operation accordingly. */
@@ -186,7 +211,7 @@ int main( int argc, char* argv[] )
         if( mp4sys_importer_get_access_unit( structs.importer, 1, sample->data, &sample->length ) )
         {
             isom_delete_sample( sample );
-            // return M4AMUX_ERR( "Failed to get a frame from input file. Maybe corrupted.\n" );
+            // return AUDIOMUX_ERR( "Failed to get a frame from input file. Maybe corrupted.\n" );
             eprintf( "Failed to get a frame from input file. Maybe corrupted.\n" );
             eprintf( "Aborting muxing operation and trying to let output be valid m4a.\n" );
             break; /* error */
@@ -202,7 +227,7 @@ int main( int argc, char* argv[] )
         sample->index = sample_entry;
         sample->prop = dependency; /* every sample is a random access point. */
         if( isom_write_sample( structs.root, track, sample ) )
-            return M4AMUX_ERR( "Failed to write a frame.\n" );
+            return AUDIOMUX_ERR( "Failed to write a frame.\n" );
         numframe++;
         eprintf( "frame = %d\r", numframe );
     }
