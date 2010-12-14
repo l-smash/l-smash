45a9f595d167d06a299d9adb90044d6b215cb839
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2010-12-15 08:03:39 +0900

    Fix justification.

--- isom.h
+++ isom.h
@@ -1125,11 +1125,11 @@ enum isom_handler_type_code
 
 enum isom_media_type_code
 {
-    ISOM_MEDIA_HANDLER_TYPE_AUDIO  = ISOM_4CC( 's', 'o', 'u', 'n' ),
+    ISOM_MEDIA_HANDLER_TYPE_AUDIO = ISOM_4CC( 's', 'o', 'u', 'n' ),
     ISOM_MEDIA_HANDLER_TYPE_VIDEO = ISOM_4CC( 'v', 'i', 'd', 'e' ),
-    ISOM_MEDIA_HANDLER_TYPE_HINT   = ISOM_4CC( 'h', 'i', 'n', 't' ),
-    ISOM_MEDIA_HANDLER_TYPE_META   = ISOM_4CC( 'm', 'e', 't', 'a' ),
-    ISOM_MEDIA_HANDLER_TYPE_TEXT   = ISOM_4CC( 't', 'e', 'x', 't' ),
+    ISOM_MEDIA_HANDLER_TYPE_HINT  = ISOM_4CC( 'h', 'i', 'n', 't' ),
+    ISOM_MEDIA_HANDLER_TYPE_META  = ISOM_4CC( 'm', 'e', 't', 'a' ),
+    ISOM_MEDIA_HANDLER_TYPE_TEXT  = ISOM_4CC( 't', 'e', 'x', 't' ),
 };
 
 enum isom_data_reference_type_code
