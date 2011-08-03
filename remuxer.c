1cf4be0abf7e6d1473bf36af6b0cac851110f4c5
Hiroki Taniura <boiled.sugar@gmail.com>
2011-08-03 21:37:39 +0900

    Implement remuxer option
    Now input requires -i and output requires -o.
    Track options can be set by -i input?[track_number1]:[track_option1],[track_option2]?[track_number2]:...

--- remuxer.c
+++ remuxer.c
@@ -26,6 +26,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
+#include <stdarg.h>
 
 #define LSMASH_MAX( a, b ) ((a) > (b) ? (a) : (b))
 
@@ -53,10 +54,24 @@ typedef struct
 
 typedef struct
 {
+    char *raw_track_option;
+    int16_t  alternate_group;
+    uint16_t ISO_language;
+} track_media_option;
+
+typedef struct
+{
     movie_t *output;
     movie_t *input;
     int num_input;
+    track_media_option **track_option;
 } movie_io_t;
+
+typedef struct
+{
+    char *whole_track_option;
+    int num_track_delimiter;
+} file_option;
 
 static void cleanup_movie( movie_t *movie )
 {
@@ -72,23 +87,33 @@ static void cleanup_movie( movie_t *movie )
     movie->track = NULL;
 }
 
-static int remuxer_error( movie_io_t *io, char* message )
+static int remuxer_error( movie_io_t *io, const char* message, ... )
 {
     for( int i = 0; i < io->num_input; i++ )
+    {
         cleanup_movie( &io->input[i] );
+        if( io->track_option[i] )
+            free( io->track_option[i] );
+    }
     cleanup_movie( io->output );
-    eprintf( message );
+    va_list args;
+    va_start( args, message );
+    vfprintf( stderr, message, args );
+    va_end( args );
     return -1;
 }
 
-static int error_message( char* message )
+static int error_message( const char* message, ... )
 {
-    eprintf( message );
+    va_list args;
+    va_start( args, message );
+    vfprintf( stderr, message, args );
+    va_end( args );
     return -1;
 }
 
-#define REMUXER_ERR( message ) remuxer_error( &io, message )
-#define ERROR_MSG( message ) error_message( message )
+#define REMUXER_ERR( ... ) remuxer_error( &io, __VA_ARGS__ )
+#define ERROR_MSG( ... ) error_message( __VA_ARGS__ )
 
 static int get_movie( movie_t *input, char *input_name )
 {
@@ -210,24 +235,144 @@ static int moov_to_front_callback( void *param, uint64_t written_movie_size, uin
     return 0;
 }
 
+static int parse_track_option( movie_io_t *io )
+{
+    track_media_option **track = io->track_option;
+    for( int i = 0; i < io->num_input; i++ )
+        for( uint32_t j = 0; j < io->input[i].num_tracks; j++ )
+        {
+            track_media_option *current_track_opt = &track[i][j];
+            if( current_track_opt->raw_track_option == NULL )
+                break;
+            if( !strchr( current_track_opt->raw_track_option, ':' )
+                || strchr( current_track_opt->raw_track_option, ':' ) == current_track_opt->raw_track_option )
+                return ERROR_MSG( "Error: track number is not specified in %s\n", current_track_opt->raw_track_option );
+            if( strchr( current_track_opt->raw_track_option, ':' ) != strrchr( current_track_opt->raw_track_option, ':' ) )
+                return ERROR_MSG( "Error: multiple colons inside one track option in %s.\n", current_track_opt->raw_track_option );
+            uint32_t track_number = atoi( strtok( current_track_opt->raw_track_option, ":" ) );
+            if( track_number == 0 )
+                return ERROR_MSG( "Error: %s is an invalid track number.\n", strtok( current_track_opt->raw_track_option, ":" ) );
+            if( track_number > io->input[i].num_tracks )
+                return ERROR_MSG( "Error: %d is an invalid track number.\n", track_number );
+            char *track_option;
+            while( (track_option = strtok( NULL, "," )) != NULL )
+            {
+                if( strchr( track_option, '=' ) != strrchr( track_option, '=' ) )
+                    return ERROR_MSG( "Error: multiple equal signs inside one track option in %s\n", track_option );
+                if( strstr( track_option, "alternate_group=" ) )
+                {
+                    char *track_parameter = strchr( track_option, '=' ) + 1;
+                    track[i][track_number - 1].alternate_group = atoi( track_parameter );
+                }
+                else if( strstr( track_option, "language=" ) )
+                {
+                    char *track_parameter = strchr( track_option, '=' ) + 1;
+                    track[i][track_number - 1].ISO_language = lsmash_pack_iso_language( track_parameter );
+                }
+                else
+                    return ERROR_MSG( "Error: unkown track option %s\n", track_option );
+            }
+        }
+    return 0;
+}
+
+static int parse_cli_option( int argc, char **argv, movie_io_t *io )
+{
+    movie_t *input = io->input;
+    track_media_option **track_option = io->track_option;
+    file_option input_file_option[io->num_input];
+    int input_movie_number = 0;
+    for( int i = 1; i < argc ; i++ )
+    {
+        /* Get input movies. */
+        if( !strcasecmp( argv[i], "-i" ) || !strcasecmp( argv[i], "--input" ) )    /* input file */
+        {
+            if( ++i == argc )
+                return ERROR_MSG( "Error: -i requires an argument.\n" );
+            input_file_option[input_movie_number].num_track_delimiter = 0;
+            char *p = argv[i];
+            while( *p )
+                input_file_option[input_movie_number].num_track_delimiter += (*p++ == '?');
+            if( get_movie( &input[input_movie_number], strtok( argv[i], "?" ) ) )
+                return ERROR_MSG( "Failed to get input movie.\n" );
+            uint32_t num_tracks = input[input_movie_number].num_tracks;
+            track_option[input_movie_number] = malloc( num_tracks * sizeof(track_media_option) );
+            if( !track_option[input_movie_number] )
+                return ERROR_MSG( "Error: couldn't allocate memory.\n" );
+            memset( track_option[input_movie_number], 0, num_tracks * sizeof(track_media_option) );
+            input_file_option[input_movie_number].whole_track_option = strtok( NULL, "" );
+            input_movie_number++;
+        }
+        /* Create output movie. */
+        else if( !strcasecmp( argv[i], "-o" ) || !strcasecmp( argv[i], "--output" ) )    /* output file */
+        {
+            if( ++i == argc )
+                return ERROR_MSG( "Error: -o requires an argument.\n" );
+            io->output->root = lsmash_open_movie( argv[i], LSMASH_FILE_MODE_WRITE );
+        }
+        else
+            return ERROR_MSG( "Unkown option found: %s\n", argv[i] );
+    }
+    /* Parse track options */
+    /* Get current track and media parameters */
+    for( int i = 0; i < io->num_input; i++ )
+        for( uint32_t j = 0; j < input[i].num_tracks; j++ )
+        {
+            track_t *in_track = &input[i].track[j];
+            track_option[i][j].alternate_group = in_track->track_param.alternate_group;
+            track_option[i][j].ISO_language = in_track->media_param.ISO_language;
+        }
+    /* Get track and media parameters specified by users */
+    for( int i = 0; i < io->num_input; i++ )
+    {
+        if( input_file_option[i].num_track_delimiter > input[i].num_tracks )
+            return ERROR_MSG( "More track options specified than the actual number of the tracks (%"PRIu32").\n", input[i].num_tracks );
+        if( input_file_option[i].num_track_delimiter )
+        {
+            track_option[i][0].raw_track_option = strtok( input_file_option[i].whole_track_option, "?" );
+            for( int j = 1; j < input_file_option[i].num_track_delimiter ; j++ )
+                track_option[i][j].raw_track_option = strtok( NULL, "?" );
+        }
+    }
+    if( parse_track_option( io ) )
+        return ERROR_MSG( "Failed to parse track options.\n" );
+    return 0;
+}
+
+static void display_help( void )
+{
+    eprintf( "Usage: remuxer -i input1 [-i input2 -i input3 ...] -o output\n" );
+    eprintf( "How to use track options:\n" );
+    eprintf( "    -i input?[track_number1]:[track_option1],[track_option2]?[track_number2]:...\n" );
+    eprintf( "For example:\n" );
+    eprintf( "    remuxer -i input1 -i input2?2:alternate_group=1?3:language=jpn,alternate_group=1 -o output\n" );
+    eprintf( "Available track options are:\n" );
+    eprintf( "    alternate_group\n" );
+    eprintf( "    language\n" );
+}
+
 int main( int argc, char *argv[] )
 {
-    if( argc < 3 || !strcasecmp( argv[1], "-h" ) || !strcasecmp( argv[1], "--help" ) )
+    if( argc < 5 || !strcasecmp( argv[1], "-h" ) || !strcasecmp( argv[1], "--help" ) )
     {
-        fprintf( stderr, "Usage: remuxer input1 [input2 input3 ...] output\n" );
-        return -1;
+        display_help();
+        return argc < 5 ? -1 : 0;
     }
-    int num_input = argc - 2;
+    int num_input = 0;
+    for( int i = 1 ; i < argc ; i++ )
+        if( !strcasecmp( argv[i], "-i" ) || !strcasecmp( argv[i], "--input" ) )
+            num_input++;
     movie_t output = { 0 };
     movie_t input[ num_input ];
-    movie_io_t io = { &output, input, num_input };
+    track_media_option *track_option[num_input];
+    movie_io_t io = { &output, input, num_input, track_option };
     memset( input, 0, num_input * sizeof(movie_t) );
-    /* Get input movies. */
-    for( int i = 0; i < num_input; i++ )
-        if( get_movie( &input[i], argv[i + 1] ) )
-            return REMUXER_ERR( "Failed to get input movie.\n" );
-    /* Create output movie. */
-    output.root = lsmash_open_movie( argv[argc - 1], LSMASH_FILE_MODE_WRITE );
+    memset( track_option, 0, num_input * sizeof(track_media_option *) );
+    if( !num_input )
+        return REMUXER_ERR( "No input file specified.\n" );
+    /* Parse command line options */
+    if( parse_cli_option( argc, argv, &io ) )
+        return REMUXER_ERR( "Failed to parse command line options.\n" );
     if( !output.root )
         return REMUXER_ERR( "Failed to open output movie.\n" );
     if( set_movie_parameters( &io ) )
@@ -244,6 +389,7 @@ int main( int argc, char *argv[] )
         return REMUXER_ERR( "Failed to alloc output tracks.\n" );
     output.current_track_number = 1;
     for( int i = 0; i < num_input; i++ )
+    {
         for( uint32_t j = 0; j < input[i].num_tracks; j++ )
         {
             track_t *in_track = &input[i].track[j];
@@ -255,6 +401,9 @@ int main( int argc, char *argv[] )
             /* Copy track and media parameters except for track_ID. */
             out_track->track_param = in_track->track_param;
             out_track->media_param = in_track->media_param;
+            /* Set track and media parameters specified by users */
+            out_track->track_param.alternate_group = track_option[i][j].alternate_group;
+            out_track->media_param.ISO_language = track_option[i][j].ISO_language;
             out_track->track_param.track_ID = out_track->track_ID;
             if( lsmash_set_track_parameters( output.root, out_track->track_ID, &out_track->track_param ) )
                 return REMUXER_ERR( "Failed to set track parameters.\n" );
@@ -267,6 +416,9 @@ int main( int argc, char *argv[] )
             out_track->reach_end_of_media_timeline = 0;
             ++ output.current_track_number;
         }
+        free( track_option[i] );
+        io.track_option[i] = NULL;
+    }
     output.current_track_number = 1;
     /* Start muxing. */
     double   largest_dts = 0;
