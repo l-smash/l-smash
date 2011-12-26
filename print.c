34c27c8064982569cdf210dd393fd1dc957ff9cb
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2011-12-26 22:51:30 +0900

    Do fflush at the end of dumping.

--- print.c
+++ print.c
@@ -2008,8 +2008,12 @@ int lsmash_print_movie( lsmash_root_t *root, const char *filename )
     {
         isom_print_entry_t *data = (isom_print_entry_t *)entry->data;
         if( !data || data->func( destination, root, data->box, data->level ) )
+        {
+            fflush( destination );
             return -1;
+        }
     }
+    fflush( destination );
     return 0;
 }
 
