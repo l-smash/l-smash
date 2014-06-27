4f4834cb884d8967874b3fbab41ab36fd514feac
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2014-06-27 10:23:15 +0900

    public header: Update micro ver. because of regular HEVC support.

--- lsmash.h
+++ lsmash.h
@@ -35,7 +35,7 @@
  ****************************************************************************/
 #define LSMASH_VERSION_MAJOR  1
 #define LSMASH_VERSION_MINOR 11
-#define LSMASH_VERSION_MICRO  7
+#define LSMASH_VERSION_MICRO  8
 
 #define LSMASH_VERSION_INT( a, b, c ) (((a) << 16) | ((b) << 8) | (c))
 
